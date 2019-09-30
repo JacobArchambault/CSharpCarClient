bf853ef1679cf839f4bc3c6de50e6269dff2e80a
Jacob Archambault <jacob.archambault@waystar.com>
2019-09-30 09:08:21 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using CarLibrary;
+
+namespace CSharpCarClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** C# Car Library Client App");
+            // Make a sports car.
+            SportsCar viper = new SportsCar("Viper", 240, 40);
+            viper.TurboBoost();
+
+            // Make a minivan.
+            MiniVan mv = new MiniVan();
+            mv.TurboBoost();
+
+            Console.WriteLine("Done. Press any key to terminate");
+            Console.ReadLine();
+        }
+    }
+}
